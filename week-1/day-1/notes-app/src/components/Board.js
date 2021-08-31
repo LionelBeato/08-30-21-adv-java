38f8508e93d061dc7060314072d578c9d182a747
Lionel Beato <lionel.beato@gmail.com>
2021-08-31 10:07:01 -0400

    continue working on notes app

--- week-1/day-1/notes-app/src/components/Board.js
+++ week-1/day-1/notes-app/src/components/Board.js
@@ -5,21 +5,52 @@ import Note from './Note';
 class Board extends Component {
     constructor() {
         super();
+        this.state = {
+            notes: [
+                {
+                    title: "Class Notes",
+                    body: "Always use constructors when extending another class",
+                },
+                {
+                    title: "Bloop",
+                    body: "blop",
+                },
+                {
+                    title: "Third title",
+                    body: "Third body",
+                },
+            ]
+        }
+    }
+
+    addNote() {
+        let notes = this.state.notes;
+        notes.push(
+            {
+                title: "New Title",
+                body: "New Body",
+            }
+        );
+        this.setState({notes});
+
     }
 
     render() {
+
+
         return (
             <div>
                 <div className="div-board">
                     <div className="row">
-                        <Note title="Class Notes" body="Always use constructors when extending another class" />
-                        <Note title="Bloop" body="blop" />
-                        <Note title="Third title" body="third body" />
-                        <Note />
+
+                        {
+                         this.state.notes.map(note => <Note title={note.title} body={note.body} />)
+                        }
+          
                     </div>
                 </div>
                 <div>
-                    <button className="btn btn-success add-button">
+                    <button onClick={this.addNote.bind(this)} className="btn btn-success add-button">
                         Add
                     </button>
                 </div>
